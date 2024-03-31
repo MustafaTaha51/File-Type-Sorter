5bed803fc0cecf745958cbf8fb707affff9d60b4
root <root@NiecLaptop>
2024-03-31 14:49:58 +1100

    GUI now interfaces with file sorting function, prompts user to choose directory to sort.

--- file_sort.py
+++ file_sort.py
@@ -3,30 +3,41 @@ from sys import argv
 from magic import Magic
 from shutil import move
 from customtkinter import *
+from tkinter import filedialog
+
+selectedPath = None
 
 app = CTk()
 app.geometry("300x400")
+state = IntVar()
 
+# Function for switching between light and dark mode
 def switch_mode():
+    # Get button state
     switchState = state.get()
+
+    # Switch mode based on current button state
     if switchState == 1:
         app._set_appearance_mode("dark")
     else:
         app._set_appearance_mode("light")
 
-btnSort = CTkButton(master=app, text="Sort", corner_radius=32, border_width=2)
 
-label = CTkLabel(master=app, text="Click To Sort Files", font=("Arial", 20),)
+# Function that prompts user to input a path and then runs sorting function
+def prompt_and_sort():
+    # Obtain file path from user
+    global filePath
+    filePath = filedialog.askdirectory()
 
-state = IntVar()
+    # Check if user gave input or cancelled
+    if not filePath:
+        print("Path was not selected")
+    else:
+        print(f"Selected path: {filePath}")
 
-switch = CTkSwitch(master=app, text="Dark Mode", variable=state, command=switch_mode)
+        # Sort files at path
+        sort_files(filePath)
 
-btnSort.place(relx=0.5, rely=0.5, anchor="center")
-switch.place(relx=0.5, rely=0.6, anchor="center")
-label.place(relx=0.5, rely=0.4, anchor="center")
-
-app.mainloop()
 
 # Function that obtains file type of file at a path
 def get_file_type(filePath):
@@ -55,42 +66,51 @@ def create_dir_move(downloadsDir, filePath, dirName, dirNames):
             move(filePath, newDirPath)
 
 
-def main():
-    # Check if command-line argument for file path was provided
-    if len(argv) > 1:
-        # List for storing directory names
-        dirNames = []
+# Sorts files at a path
+def sort_files(selectedPath):
+    # List for storing directory names
+    dirNames = []
 
-        # Define the path to the Downloads folder in Windows
-        downloadsDir = argv[1]
+    # Define the path to the Downloads folder in Windows
+    downloadsDir = selectedPath
 
-        # List all files in the Downloads directory, catch exception if path doesn't exist
+    # List all files in the Downloads directory, catch exception if path doesn't exist
+    try:
+        filesInDownloads = os.listdir(downloadsDir)
+    except FileNotFoundError:
+        print("File path was not found")
+        return
+
+    # Determine the file types of each file
+    for fileName in filesInDownloads:
+        # Join filename with path
+        filePath = os.path.join(downloadsDir, fileName)
+    
+        # If a directory is detected, ignore it, otherwise get_file_type
         try:
-            filesInDownloads = os.listdir(downloadsDir)
-        except FileNotFoundError:
-            print("File path was not found")
-            return
-
-        # Determine the file types of each file
-        for fileName in filesInDownloads:
-            # Join filename with path
-            filePath = os.path.join(downloadsDir, fileName)
+            fileType = get_file_type(filePath)
+        except IsADirectoryError:
+            fileType = None
         
-            # If a directory is detected, ignore it, otherwise get_file_type
-            try:
-                fileType = get_file_type(filePath)
-            except IsADirectoryError:
-                fileType = None
-            
-            # Run create_dir_move
-            create_dir_move(downloadsDir, filePath, fileType, dirNames)
+        # Run create_dir_move
+        create_dir_move(downloadsDir, filePath, fileType, dirNames)
 
-        print("Sorted!")
-    else:
-        # Alert user about command-line argument usage
-        print("Usage: python3 script_name.py <file_path>")
-        print("Please provide the required command-line argument.")
+    print("Sorted!")
 
 
-if __name__ == '__main__':
-    main()
+btnSort = CTkButton(master=app, text="Sort", corner_radius=32, border_width=2, command=prompt_and_sort)
+label = CTkLabel(master=app, text="Click To Sort Files", font=("Arial", 20),)
+switch = CTkSwitch(master=app, text="Dark Mode", variable=state, command=switch_mode)
+
+btnSort.place(relx=0.5, rely=0.5, anchor="center")
+switch.place(relx=0.5, rely=0.6, anchor="center")
+label.place(relx=0.5, rely=0.4, anchor="center")
+
+app.mainloop()
+
+
+
+
+
+
+
